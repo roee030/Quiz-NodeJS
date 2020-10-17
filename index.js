68e23792062ba5c089afea4c73946592c86f82f7
Roee Angel <roee.angel93@gmail.com>
2020-10-17 22:17:58 +0300

    get user q & a

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const users = require("./db/users.json");
+const questions = require("./db/questions.json");
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
@@ -9,3 +11,20 @@ app.listen(PORT, () => {
 //Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//REST API
+app.get("/", (req, res) => {
+  res.send("<h1>Hello, This is NodeJS App</h1>");
+});
+
+app.get("/quiz/results/:username", (req, res) => {
+  const userName = req.params;
+  const user = users.filter((user) => {
+    user.userName === userName;
+  });
+  if (user) {
+    res.status(200).json(user.userResults);
+  } else {
+    res.status(400).send(`There is no user like ${req.params}`);
+  }
+});
